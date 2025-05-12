5fa08be82448bff82d9c0029492709aa8963e0c3
Sailesh Dhungana <dhunganasailesh@gmail.com>
2025-05-12 12:21:00 +0545

    Added models

--- agents_for_all/llms/__init__.py
+++ agents_for_all/llms/__init__.py
@@ -1,8 +1,8 @@
-from agents_for_all.llms.anthropic import AnthropicModel
 from agents_for_all.llms.base_model import Model
 from agents_for_all.llms.direct import DirectModel
 from agents_for_all.llms.gemini import GeminiModel
 from agents_for_all.llms.openai import OpenAIModel
+from agents_for_all.llms.anthropic import AnthropicModel
 
 __all__ = [
     "Model",
